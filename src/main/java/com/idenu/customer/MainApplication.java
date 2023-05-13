682c64c0c5722ccc399d147aee1a58b79f1b7bc0
799993 <peter.idenu@aa.com>
2023-05-13 16:24:21 -0500

    implemented tomcat and generate jar

--- src/main/java/com/idenu/customer/MainApplication.java
+++ src/main/java/com/idenu/customer/MainApplication.java
@@ -1,11 +1,19 @@
 package com.idenu.customer;
 
+import com.idenu.customer.controller.CustomerController;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@ImportResource("application.xml")
+
 public class MainApplication {
 
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+
+        //Get customer controller from application context
+        CustomerController customerController = context.getBean(CustomerController.class);
+        ((ClassPathXmlApplicationContext) context).close();
 
     }
 }
